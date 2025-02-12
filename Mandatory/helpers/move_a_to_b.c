c2bfcef5764d06249b34302a5e8e861d48072571
iaskour <iaskour@student.1337.ma>
2025-02-12 08:40:45 +0100

    norminette

--- Mandatory/helpers/move_a_to_b.c
+++ Mandatory/helpers/move_a_to_b.c
@@ -1,12 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move_a_to_b.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iaskour <iaskour@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 08:33:46 by iaskour           #+#    #+#             */
+/*   Updated: 2025/02/12 08:37:49 by iaskour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../push_swap.h"
-#include <stdio.h>
 
-
-void process_for_push (t_list **stack, t_list *top_node, char stack_name)
+void	process_for_push(t_list **stack, t_list *top_node, char stack_name)
 {
 	while (*stack != top_node)
 	{
-		if(stack_name == 'a')
+		if (stack_name == 'a')
 		{
 			if (top_node->above_median)
 				ra(stack, 1);
@@ -15,7 +25,7 @@ void process_for_push (t_list **stack, t_list *top_node, char stack_name)
 		}
 		else if (stack_name == 'b')
 		{
-			if(top_node->above_median)
+			if (top_node->above_median)
 				rb(stack, 1);
 			else
 				rrb(stack, 1);
@@ -23,46 +33,46 @@ void process_for_push (t_list **stack, t_list *top_node, char stack_name)
 	}
 }
 
-t_list *get_cheapest_node (t_list *stack)
+t_list	*get_cheapest_node(t_list *stack)
 {
 	while (stack)
 	{
 		if (stack->cheapest == 1)
-			return stack;
+			return (stack);
 		stack = stack->next;
 	}
-	return NULL;
+	return (NULL);
 }
 
-void rotate_both (t_list **stack_a, t_list **stack_b, t_list *cheapest_node)
+void	rotate_both(t_list **stack_a, t_list **stack_b, t_list *cheapest_node)
 {
 	while (*stack_a != cheapest_node && *stack_b != cheapest_node->target_node)
 		rr(stack_a, stack_b);
-
 	set_index_median(*stack_a);
 	set_index_median(*stack_b);
 }
 
-void reverse_rotate_both (t_list **stack_a, t_list **stack_b, t_list *cheapest_node)
+void	reverse_rotate_both(t_list **stack_a,
+		t_list **stack_b, t_list *cheapest_node)
 {
 	while (*stack_a != cheapest_node && *stack_b != cheapest_node->target_node)
 		rrr(stack_a, stack_b);
-
 	set_index_median(*stack_a);
 	set_index_median(*stack_b);
 }
 
-void move_a_to_b (t_list **stack_a , t_list **stack_b)
+void	move_a_to_b(t_list **stack_a, t_list **stack_b)
 {
+	t_list	*cheapest_node;
 
-
-	t_list *cheapest_node = get_cheapest_node(*stack_a);
-	if(!cheapest_node)
+	cheapest_node = get_cheapest_node(*stack_a);
+	if (!cheapest_node)
 		return ;
-	
-	if (cheapest_node->above_median == 1 && cheapest_node->target_node->above_median == 1)
+	if (cheapest_node->above_median == 1
+		&& cheapest_node->target_node->above_median == 1)
 		rotate_both(stack_a, stack_b, cheapest_node);
-	else if (cheapest_node->above_median == 0 && cheapest_node->target_node->above_median == 0)
+	else if (cheapest_node->above_median == 0
+		&& cheapest_node->target_node->above_median == 0)
 		reverse_rotate_both(stack_a, stack_b, cheapest_node);
 	process_for_push(stack_a, cheapest_node, 'a');
 	process_for_push(stack_b, cheapest_node->target_node, 'b');
--- Mandatory/helpers/move_b_to_a.c
+++ Mandatory/helpers/move_b_to_a.c
@@ -6,13 +6,13 @@
 /*   By: iaskour <iaskour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 08:28:48 by iaskour           #+#    #+#             */
-/*   Updated: 2025/02/12 08:28:56 by iaskour          ###   ########.fr       */
+/*   Updated: 2025/02/12 08:38:47 by iaskour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void move_b_to_a (t_list **stack_a, t_list **stack_b)
+void	move_b_to_a(t_list **stack_a, t_list **stack_b)
 {
 	process_for_push(stack_a, (*stack_b)->target_node, 'a');
 	pa(stack_a, stack_b);
